605ab94069d49665dd6a4bb27f8b242d9fb9e026
CairoVieira <vieira.cairo@hotmail.com>
2020-11-11 14:07:21 +0000

    (11/11) Término do comp. de listagem e início do comp. de formulário

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
+import { CursoFormComponent } from './curso/curso-form/curso-form.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CursoListComponent } from './curso/curso-list/curso-list.component';
 
 const routes: Routes = [
-    { path: 'curso', component: CursoListComponent}
+    { path: 'curso', component: CursoListComponent},
+    { path: 'curso/novo', component: CursoFormComponent},
+    { path: 'curso/:id', component: CursoFormComponent}
 ];
 
 @NgModule({
