ffea712eb08c31531b5c028e18e71ef218783147
CairoVieira <vieira.cairo@hotmail.com>
2020-11-25 14:08:10 +0000

    (25/11) Início de formulário com relacionamentos

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { TurmaFormComponent } from './turma/turma-form/turma-form.component';
 import { CursoFormComponent } from './curso/curso-form/curso-form.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -8,7 +9,9 @@ const routes: Routes = [
     { path: 'curso', component: CursoListComponent},
     { path: 'curso/novo', component: CursoFormComponent},
     { path: 'curso/:id', component: CursoFormComponent},
-    { path: 'turma', component: TurmaListComponent}
+    { path: 'turma', component: TurmaListComponent},
+    { path: 'turma/nova', component: TurmaFormComponent},
+    { path: 'turma/:id', component: TurmaFormComponent}
 ];
 
 @NgModule({
